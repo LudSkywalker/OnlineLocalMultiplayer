f9c1a1e45f9ac00bbb148e41787390d0ee14136f
LudSkywalker <ludingnumpaque@gmail.com>
2021-06-07 20:24:36 -0500

    Creation of the proyect

--- client.py
+++ client.py
@@ -2,19 +2,27 @@ import socketio
 import pyautogui
 # standard Python
 sio = socketio.Client()
-# sio.connect('https://lindura.herokuapp.com')
-sio.connect('http://localhost:3000')
-
+sio.connect('https://lindura.herokuapp.com')
+#sio.connect('http://localhost:3000')
+nd=0
+nu=0
 
 @sio.on('keyd')
 def on_key(key):
-    if(key != 16):
-        print(chr(key).lower())
+    global nd
+    if(key != 16 and nd<6):
+        nd+=1
+        #print(chr(key).lower())
         pyautogui.keyDown(chr(key).lower())
+        nd-=1
 
 
 @sio.on('keyu')
 def on_key(key):
-    if(key != 16):
-        print(chr(key).lower())
+    global nu
+    if(key != 16 and nu<6):
+        nu+=1
+        #print(chr(key).lower())
         pyautogui.keyUp(chr(key).lower())
+        nu-=1
+    
